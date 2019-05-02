79a53e00c80acee680be1572de3d93a30708b6de
eric-do <ericdo.617@gmail.com>
2019-05-02 12:30:26 -0700

    Add message

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,10 +1,14 @@
 var models = require('../models');
+var db = require('../db');
 
 module.exports = {
   messages: {
     get: function (req, res) {}, // a function which handles a get request for all messages
     post: function (req, res) {
       console.log('hi messages');
+      db.addMessage(req.body, (err, data) => {
+        res.end();
+      });
     } // a function which handles posting a message to the database
   },
   
@@ -12,8 +16,10 @@ module.exports = {
     // Ditto as above
     get: function (req, res) {},
     post: function (req, res) {
-      console.log('Hi usersssss');
-      console.log(typeof req.body);
+      db.addUser(req.body.username, (err, data) => {
+        res.end();
+      });
+
     }
   }
 };
--- server/db/index.js
+++ server/db/index.js
@@ -4,24 +4,65 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-// dbConnection = mysql.createConnection({
-//     user: 'root',
-//     database: 'chat'
-//   });
-//   dbConnection.connect();
-
-//   dbConnection.query('truncate ' + messagesTable, done);
+db = mysql.createConnection({
+    user: 'student',
+    password: 'student',
+    database: 'chat'
+  });
+  db.connect();
 
 // Establish database connection
+module.exports = {
+  addUser: (user, callback) => {
+    //Input: user as string and then callback function
+    //return nothing
+    // Query DB
+    //  If user exists: callback (null, data)
+    //  Else: insert user into table
+    //   on complete query table
+    //   finally execute CB
+
+    db.query(`INSERT INTO users (username) VALUES ('${user}')`, (err, data) => {
+      if (err) { throw new Error ('error insert user'); }
+
+      db.query( `SELECT * FROM users`, (err, data) => {
+        if (err){ throw new Error (' error query table'); }
+        console.log(JSON.stringify(data));
+        callback(null, data);
+      });
+    });
+  },
+
+  addMessage: (message, callback) => {
+    // input is a message obj with attributes user msg and room
+    // return nothing
+    // Get user id, which needs a callback function
+    // In our callback function, we will insert message to messages table
+    //  Messages, room, and link the user id to the user_id FK
+    
+    module.exports.getUserId(message.username, (err, user_id) => {
+      db.query(`INSERT INTO messages (text_msg, room, user_id) 
+                VALUES ('${message.message}', '${message.roomname}', '${user_id}')`, (err, data) =>{
+                callback(null, data);
+      })
+    });
+  
+
+
+  },
+  
+  getUserId: (username, callback) => {
+    db.query(`SELECT * from users WHERE username = '${username}'`, (err, data) => {
+      if (err) { throw new Error ('[mysql error] error querying for user'); }
+      console.log(data.username, data.user_id);
+      callback(null, data.user_id);
+    })
+  }
+
+}
+
+
 
 
 /* Functions */
-var addUser = (user) => {
-    // Input: this function takes a username
-    // Take username from param
-    // Call mysql query function
-    // Insert user into table
-    // TEST: query for user in table
-
-}
 //dbConnection.query('SELECT * FROM users', done);
