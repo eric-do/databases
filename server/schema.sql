d08d628aa3cda1be4c02cda2e413e1a37f8e5106
eric-do <ericdo.617@gmail.com>
2019-05-01 17:10:40 -0700

    Update schema.sql file with 2 tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,25 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+/* Create other tables and define schemas for them here! */
+-- TODO: figure out if AUTO_INCREMENT is doable in scope of project
+CREATE TABLE users (
+  user_id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20) NOT NULL,
+  PRIMARY KEY (user_id)
 );
 
-/* Create other tables and define schemas for them here! */
+-- TODO: figure out if AUTO_INCREMENT is doable in scope of project
+CREATE TABLE messages (
+  id INT NOT NULL AUTO_INCREMENT,
+  text_msg VARCHAR(255) NOT NULL,
+  room VARCHAR(20) NOT NULL,
+  user_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
 
 
 
